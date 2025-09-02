67ebb845fecc21a4b2adc87fdbafe65dbbf6c38d
tingmei <192350003+tingmei936@users.noreply.github.com>
2025-09-02 17:18:41 +0200

    chore: remove MyHttpOverrides

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,12 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 
 import 'core/core.dart';
 import 'event_app.dart';
 
-class MyHttpOverrides extends HttpOverrides {
-  @override
-  HttpClient createHttpClient(SecurityContext? context) {
-    return super.createHttpClient(context)
-      ..badCertificateCallback =
-          (X509Certificate cert, String host, int port) => true;
-  }
-}
-
 /// Entry point of the Flutter application for tech events
 /// Initializes configuration and data loader before running the app
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  HttpOverrides.global = MyHttpOverrides();
 
   final config = await ConfigLoader.loadConfig();
   final organization = await ConfigLoader.loadOrganization();
