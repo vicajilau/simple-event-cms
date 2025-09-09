443a2a4702206a5f5fa273d214d0b3e13eebbd51
FranCedron2690 <9269823+FranCedron2690@users.noreply.github.com>
2025-09-09 17:38:25 +0200

    Moved widget

--- lib/ui/screens/event_collection_screen.dart
+++ lib/ui/screens/event_collection_screen.dart
@@ -10,49 +10,6 @@ import '../../core/models/organization.dart';
 import '../widgets/widgets.dart';
 import 'screens.dart';
 
-class FilterCheckbox extends StatefulWidget {
-  final String label;
-  final bool isChecked;
-  final void Function(bool) onChanged;
-  const FilterCheckbox({
-    super.key,
-    required this.isChecked,
-    required this.label,
-    required this.onChanged,
-  });
-
-  @override
-  State<FilterCheckbox> createState() => _FilterCheckboxState();
-}
-
-class _FilterCheckboxState extends State<FilterCheckbox> {
-  bool isChecked = false;
-
-  @override
-  void initState() {
-    super.initState();
-    isChecked = widget.isChecked;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Text(widget.label),
-        Checkbox(
-          value: isChecked,
-          onChanged: (bool? value) {
-            setState(() {
-              isChecked = value!;
-              widget.onChanged(value);
-            });
-          },
-        ),
-      ],
-    );
-  }
-}
-
 /// Main home screen widget that displays the event information and navigation
 /// Features a bottom navigation bar with tabs for Agenda, Speakers, and Sponsors
 class EventCollectionScreen extends StatefulWidget {
