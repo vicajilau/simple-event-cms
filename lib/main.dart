486d6f061a4dc8afd8987061b3ed87f9ee1b010f
Daniel Jimenez <122808735+daniJimen@users.noreply.github.com>
2025-09-09 15:45:21 +0200

    fix issue click buttons into main

--- lib/main.dart
+++ lib/main.dart
@@ -5,39 +5,53 @@ import 'package:sec/ui/screens/login_screen.dart';
 void main() {
   usePathUrlStrategy();
   runApp(MyApp());
+
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp( // MaterialApp provides the Navigator
       title: 'Login con GitHub',
-      home: Scaffold(
-        appBar: AppBar(title: Text('Login con GitHub')),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(
-              child: ElevatedButton(
-                child: Text('Iniciar sesión con GitHub'),
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => LoginPage()));
-                }
-              ),
+      home: MyHomePage(), // Use a separate widget for the home screen
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Login con GitHub')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: ElevatedButton(
+              child: Text('Iniciar sesión con GitHub'),
+              onPressed: () {
+                Navigator.push( // Now this context has a Navigator
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginPage()),
+                );
+              },
             ),
-            SizedBox(height: 20), // Espacio entre los botones
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => LoginPage()));
-                },
-                child: Text('Modo Invitado'),
-              ),
+          ),
+          SizedBox(height: 20), // Espacio entre los botones
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push( // And this context also has a Navigator
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginPage()),
+                );
+              },
+              child: Text('Modo Invitado'),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
