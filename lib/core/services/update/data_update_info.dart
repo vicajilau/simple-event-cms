c6f8ac7981b83b0ee1b5e41b58c38d5359d83492
Daniel Jimenez <122808735+daniJimen@users.noreply.github.com>
2025-09-10 11:02:15 +0200

    Feat: Add empty list checks for data updates
    This commit adds checks to the `updateSpeakers`, `updateAgenda`, `updateSponsors`, and `updateEvents` methods in `DataUpdateInfo.dart`.
    
    If the list of data to update is empty, an exception will be thrown. This prevents unnecessary API calls and potential errors.

--- lib/core/services/update/data_update_info.dart
+++ lib/core/services/update/data_update_info.dart
@@ -11,6 +11,9 @@ class DataUpdateInfo {
   /// Loads speaker information from the speakers.json file
   /// Returns a Future containing a list of speaker data
   Future<http.Response> updateSpeakers(List<Speaker> speakers) async {
+    if (speakers.isEmpty) {
+      throw Exception("No speakers to update");
+    }
     return dataCommons.updateData(
       speakers,
       speakers[0].pathUrl,
@@ -23,26 +26,36 @@ class DataUpdateInfo {
   /// with proper type conversion and validation
   /// Returns a Future containing a list of AgendaDay models
   Future<http.Response> updateAgenda(List<Agenda> agenda) async {
-    return dataCommons.updateData(
-      agenda,
-      agenda[0].pathUrl,
-      agenda[0].updateMessage,
-    );
+    if (agenda.isEmpty) {
+      throw Exception("No agenda to update");
+    }
+      return dataCommons.updateData(
+        agenda,
+        agenda[0].pathUrl,
+        agenda[0].updateMessage,
+      );
+
   }
 
   /// Loads sponsor information from the sponsors.json file
   /// Returns a Future containing a list of sponsor data with logos and details
   Future<http.Response> updateSponsors(List<Sponsor> sponsors) async {
-    return dataCommons.updateData(
-      sponsors,
-      sponsors[0].pathUrl,
-      sponsors[0].updateMessage,
-    );
+    if (sponsors.isEmpty) {
+      throw Exception("No sponsors to update");
+    }
+      return dataCommons.updateData(
+        sponsors,
+        sponsors[0].pathUrl,
+        sponsors[0].updateMessage,
+      );
   }
 
   /// Update events information from the events.json file
   /// Returns a Future containing a list of events data with logos and details
   Future<http.Response> updateEvents(List<Event> events) async {
+    if(events.isEmpty) {
+      throw Exception("No events to update");
+    }
     return dataCommons.updateData(
       events,
       events[0].pathUrl,
