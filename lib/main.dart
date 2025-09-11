b441b09ce4bf732155e435996bbf1a6fa38ee59b
Victor Carreras <34163765+vicajilau@users.noreply.github.com>
2025-09-11 15:59:35 +0200

    Add flutter_web_plugins as a direct dependency and configure clean URLs for Flutter web

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:sec/core/di/dependency_injection.dart';
 import 'package:sec/event_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  
+  // Configurar URLs limpias sin hash para Flutter web
+  usePathUrlStrategy();
 
   // Inicializar dependencias
   await setupDependencies();
