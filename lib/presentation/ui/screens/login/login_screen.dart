85b1ee9e09bc16b73a14c58821f15884ac916694
Daniel Jimenez <122808735+daniJimen@users.noreply.github.com>
2025-09-17 12:06:15 +0200

    Fix: Specify classic token on login screen
    Updates the label of the token input field from "Token" to "Token (classic)" to clarify what type of GitHub token is required.

--- lib/presentation/ui/screens/login/login_screen.dart
+++ lib/presentation/ui/screens/login/login_screen.dart
@@ -106,7 +106,7 @@ class _LoginPageState extends State<LoginScreen> {
               ),
               const SizedBox(height: 20),
               TextFormField(
-                decoration: const InputDecoration(labelText: 'Token'),
+                decoration: const InputDecoration(labelText: 'Token(classic with write permissions)'),
                 validator: (value) =>
                     value!.isEmpty ? 'Please enter a valid GitHub token' : null,
                 onSaved: (value) => _token = value!,
