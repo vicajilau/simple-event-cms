1b80c5f8b42b65008fb9f28f760dc202a3c46b80
Daniel Jimenez <122808735+daniJimen@users.noreply.github.com>
2025-09-29 08:37:31 +0200

    Pass eventId to speaker and sponsor creation
    This commit updates `EventDetailScreen` to pass the `eventId` when creating new speakers and sponsors.
    
    - `_addSpeaker` now receives `widget.eventId`.
    - `_addSponsor` now receives `widget.eventId`.

--- lib/presentation/ui/screens/event_detail/event_detail_screen.dart
+++ lib/presentation/ui/screens/event_detail/event_detail_screen.dart
@@ -121,9 +121,9 @@ class _EventDetailScreenState extends State<EventDetailScreen>
                 if (_selectedIndex == 0) {
                   _addTrackToAgenda();
                 } else if (_selectedIndex == 1) {
-                  _addSpeaker("");//TODO not have parent id
+                  _addSpeaker(widget.eventId);//TODO not have parent id
                 } else if (_selectedIndex == 2) {
-                  _addSponsor("");//TODO not have parent id
+                  _addSponsor(widget.eventId);//TODO not have parent id
                 }
               },
             );
